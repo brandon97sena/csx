2a452bea85fa314ed05b76527d7609ed871529ba
Francine <francine.t.karpeh@gmail.com>
2019-09-30 21:09:25 -0400

    new file for downloads

---
+++ mp_1/week_0/downloads.md
@@ -0,0 +1,9 @@
+## Here is an intro to our downloads for the CSX program
+
+##### Install Git
+⋅⋅1. insert link here
+
+##### Install Python & pip
+⋅⋅1.
+
+#####
---
+++ mp_1/week_1/2_vartypes_todo.py
@@ -0,0 +1,13 @@
+# TODO: make a string and concatenate it with numbers, then print the result
+string1 = "Hello, " 
+string2 = "my name is Franny "
+string3 = "369"
+
+print (string1 + string2 + string3)
+#TODO: set a float as a variable and then print that float
+float2 = 8.9
+print (float2)
+# TODO: use the function type(variable-here) to print all the types we have worked with thus far
+print (type(100))
+print (type(string2))
+print (type (string3)) 
